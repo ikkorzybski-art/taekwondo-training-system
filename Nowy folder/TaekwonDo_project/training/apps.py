852d0c98693ea27c33418c4bdb5f212b027ec9da
Twoje Imię <twoj-email@example.com>
2026-01-14 11:46:34 +0100

    Commit: synchronizacja projektu  zatwierdzenie bieżących zmian

--- Nowy folder/TaekwonDo_project/training/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class TrainingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'training'
-    verbose_name = 'System Treningów Taekwon-Do'
-    
-    def ready(self):
-        import training.models  # Ładowanie sygnałów
